63930a802f2c98f9b9d68374b81113021791cbd2
RyanCrasta <ryan4121999@gmail.com>
2024-06-27 13:43:47 +0530

    add fetch logic and display

--- src/background/background.ts
+++ src/background/background.ts
@@ -6,6 +6,17 @@ import { addMovie } from "./actions";
 const store = createStore(reducer);
 let tabUpdated = false;
 
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxM2ZlNDBmNGE5Y2JhZDlhM2VlNjNhOTJlMWViYjJkZiIsIm5iZiI6MTcxOTQ2Mzk2OS4xODA2OTEsInN1YiI6IjY2N2FiY2ZmNGY1NGI2YmMwNDhiZTQ4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rbJS5aF5MykuEhQMyI-36cqHaUl7ZimtBsV_BR6D0BI",
+  },
+};
+
+const url = "https://api.themoviedb.org/3/search/movie?query=";
+
 function searchUpdated(requestDetails, changeInfo, tab) {
   if (
     tab.status === "complete" &&
@@ -22,18 +33,44 @@ function searchUpdated(requestDetails, changeInfo, tab) {
         tabUpdated = true;
         const newArr = [tab.title];
 
-        store.dispatch(
-          addMovie({
-            movieName: tab.title,
-          })
-        );
+        const newURL = `${url}${
+          tab.title.split(" - ")[0]
+        }&language=en-US&page=1`;
 
-        chrome.storage.local
-          .set({
-            urls: newArr,
+        fetch(newURL, options)
+          .then((res) => {
+            return res.json();
           })
-          .then(() => {
-            tabUpdated = true;
+          .then((json) => {
+            if (json.results.length > 0) {
+              json.results.map((result) => {
+                store.dispatch(
+                  addMovie({
+                    movieName: result.title,
+                    release_date: result.release_date,
+                    overview: result.overview,
+                    vote_average: Math.round(result.vote_average * 10) / 10,
+                    id: result.id,
+                  })
+                );
+
+                chrome.storage.local
+                  .set({
+                    urls: newArr,
+                  })
+                  .then(() => {
+                    tabUpdated = true;
+                  });
+              });
+            } else {
+              chrome.storage.local
+                .set({
+                  urls: newArr,
+                })
+                .then(() => {
+                  tabUpdated = true;
+                });
+            }
           });
       } else {
         if (
@@ -44,18 +81,44 @@ function searchUpdated(requestDetails, changeInfo, tab) {
           tabUpdated = false;
           URL.urls.push(tab.title);
 
-          store.dispatch(
-            addMovie({
-              movieName: tab.title,
-            })
-          );
+          const newURL = `${url}${
+            tab.title.split(" - ")[0]
+          }&language=en-US&page=1`;
 
-          chrome.storage.local
-            .set({
-              urls: URL.urls,
+          fetch(newURL, options)
+            .then((res) => {
+              return res.json();
             })
-            .then(() => {
-              tabUpdated = true;
+            .then((json) => {
+              if (json.results.length > 0) {
+                json.results.map((result) => {
+                  store.dispatch(
+                    addMovie({
+                      movieName: result.title,
+                      release_date: result.release_date,
+                      overview: result.overview,
+                      vote_average: Math.round(result.vote_average * 10) / 10,
+                      id: result.id,
+                    })
+                  );
+
+                  chrome.storage.local
+                    .set({
+                      urls: URL.urls,
+                    })
+                    .then(() => {
+                      tabUpdated = true;
+                    });
+                });
+              } else {
+                chrome.storage.local
+                  .set({
+                    urls: URL.urls,
+                  })
+                  .then(() => {
+                    tabUpdated = true;
+                  });
+              }
             });
         }
       }
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -4,11 +4,28 @@ import { connect } from "react-redux";
 import * as actions from "./actions";
 
 const Popup = ({ movies }) => {
-  console.log("QQQQQQQQQQQQQQ", movies);
-
   return (
     <div>
       <h1 className="heading">Movie List</h1>
+
+      {movies.map((oneMovie) => {
+        return (
+          <div className="movieDetail" key={oneMovie.id}>
+            <div className="movieName">
+              <span>Movie name:</span> {oneMovie.movieName}
+            </div>
+            <div className="movieOverview">
+              <span>Overview:</span> {oneMovie.overview}
+            </div>
+            <div className="movieReleaseDate">
+              <span>Release date:</span> {oneMovie.release_date}
+            </div>
+            <div className="movieRating">
+              <span>Rating:</span> {oneMovie.vote_average}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
